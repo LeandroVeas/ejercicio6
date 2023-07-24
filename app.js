e4f4e3554ef344c9f0d3135ef34dc0016481cbf0
Leandro Veas <leandroveasg@gmail.com>
2023-07-24 20:32:05 -0300

    Se crea el archivo

---
+++ app.js
@@ -0,0 +1,49 @@
+//Realizar un programa en Javascript que cumpla con los siguientes puntos:
+//a) Crear una variable con un valor numérico libre:
+
+
+var cantidadDeGatos = 5;
+var contador = 0;
+
+
+while(contador<cantidadDeGatos){
+
+if(cantidadDeGatos==1){
+    contador++
+    console.log(`Gato # ${contador}: 😺`) 
+}
+
+else if(cantidadDeGatos ==2 ){
+    contador++
+    console.log(`Gato # ${contador}: 😺`) 
+    contador++
+    console.log(`Gato # ${contador}: 😸`) 
+}else if(cantidadDeGatos ==3){
+    contador++
+    console.log(`Gato # ${contador}: 😺`) 
+    contador++
+    console.log(`Gato # ${contador}: 😸`) 
+    contador++
+    console.log(`Gato # ${contador}: 😹`) 
+    
+}else {
+    contador++
+    console.log(`Gato # ${contador}: 😺`) 
+    contador++
+    console.log(`Gato # ${contador}: 😸`) 
+    contador++
+    console.log(`Gato # ${contador}: 😹`) 
+    
+    
+    
+}
+}
+
+
+ 
+
+
+
+
+/* console.log(`Gato # ${contador}: 😺`)  */
+
---
+++ app2.js
@@ -0,0 +1,15 @@
+//b) Crear 2 variables con valores numéricos libres:
+
+var cantidadDeGatos =5 ;
+var cantidadDePasos = 8;
+var pasos = "🐾"
+
+
+for( let i = 1; i<=cantidadDeGatos; i = i+1){
+      var  pasosDegatos= "";
+      for( var j= 1; j<= cantidadDePasos;j=j+1){
+        pasosDegatos += pasos
+      }
+
+      console.log(`Gato # ${i}: 🐈  ${pasosDegatos}`)
+}
