b3e9f09ebbee604ffda37ef6315bfbfa9d97e56c
Leandro Veas <leandroveasg@gmail.com>
2023-07-24 21:54:43 -0300

    Se finaliza el 2 script

--- app2.js
+++ app2.js
@@ -1,15 +1,13 @@
 //b) Crear 2 variables con valores numéricos libres:
 
-var cantidadDeGatos =5 ;
-var cantidadDePasos = 8;
-var pasos = "🐾"
+var cantidadDeGatos = 5;
+var cantidadDePasos = 2;
+var pasos = "🐾";
 
-
-for( let i = 1; i<=cantidadDeGatos; i = i+1){
-      var  pasosDegatos= "";
-      for( var j= 1; j<= cantidadDePasos;j=j+1){
-        pasosDegatos += pasos
-      }
-
-      console.log(`Gato # ${i}: 🐈  ${pasosDegatos}`)
+for (let i = 1; i <= cantidadDeGatos; i = i + 1) {
+    var pasosDegatos = "";
+    for (var j = 1; j <= cantidadDePasos; j = j + 1) {
+        pasosDegatos += pasos;
+    }
+    console.log(`Gato # ${i}: 🐈  ${pasosDegatos}`);
 }
